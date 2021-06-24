408e658389d132a7a026b5e9debc0e4f9945c915
q <root@gitlab.local>
2021-06-24 05:18:16 -0500

    day 18 (before p2 first attempt cleanup)

--- aoc18/c.cpp
+++ aoc18/c.cpp
@@ -3,11 +3,13 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <queue>
 
 using namespace std;
 
 struct c{
   int p = 0;
+  int s = 0;
   map<char,long long int> r;
 };
 
@@ -55,17 +57,89 @@ bool step(c& o, vector<string>& i, int& x){
   return true;
 }
 
+bool step2(c& a, c& b, vector<string>& i, int& x, queue<long long int>& ax, queue<long long int>& bx){
+  stringstream s(i[a.p]);
+  string t;
+  char m;
+  s >> t >> m;
+  if(t == "snd"){
+    ax.push(pre(a.r,m));
+    a.s++;
+  } else if(t == "rcv"){
+    if(!bx.size()) return false;
+    a.r[m] = bx.front();
+    bx.pop();
+  } else {
+    return step(a,i,x);
+  }
+  a.p++;
+  return true;
+}
+
+bool term(c& a, vector<string>& i){
+  return a.p < 0 || a.p >= i.size();
+}
+
+bool test(c& a, c& b, vector<string>& i, queue<long long int>& ax, queue<long long int>& bx){
+  return term(a,i) || term(b,i) || (i[a.p][0] == 'r' && bx.size() == 0 && i[b.p][0] == 'r' && ax.size() == 0);
+}
+
+bool sorted(vector<int>& b){
+  for(int i = 0; i < b.size() - 1; i++) if(b[i] < b[i+1]) return false;
+  return true;
+}
+
 int main(){
   ifstream f("input");
   string l;
   vector<string> i;
   while(getline(f,l)) i.push_back(l);
 
-  c o;
+//  c o;
   int x = 0;
-
-  while(step(o, i, x));
+ // while(step(o, i, x));
   cout << x << endl;
+
+//  c a;
+//  c b;
+//  b.r['p'] = 1;
+//  queue<long long int> ax;
+//  queue<long long int> bx;
+//
+//  while(!test(a,b,i,ax,bx)){
+//    step2(a,b,i,x,ax,bx);
+//    step2(b,a,i,x,bx,ax);
+//  }
+//
+//  if(!term(b,i)) while(step2(b,a,i,x,bx,ax));
+//
+//  cout << b.s << endl;
+//
+//
+
+  vector<int> b;
+  long long int a = 1;
+  for(int i = 0; i < 31; i++) a *= 2;
+  a--;
+  long long int p = 622;
+  for(int i = 0; i < 127; i++){
+    p = (p * 8505 * 129749 + 12345) % a;
+    b.push_back(p % 10000);
+  }
+
+  int o = 2;
+  while(!sorted(b)){
+    o++;
+    for(int i = 0; i < b.size() - 1; i++){
+      if(b[i] < b[i+1]){
+        int t = b[i];
+        b[i] = b[i+1];
+        b[i+1] = t;
+      }
+    }
+  }
+
+  cout << (o/2) * 127 << endl;
 
   return 0;
 }
